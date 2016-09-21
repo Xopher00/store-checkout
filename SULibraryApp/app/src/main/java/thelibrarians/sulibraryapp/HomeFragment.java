420909a06700056996f053248532f94cb6c87188
ACER\njraf_000 <njrafala@hotmail.com>
2016-09-21 19:55:45 -0400

    Added custom toolbar/app bar; created navigation menu; able to switch fragments

--- SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/HomeFragment.java
+++ SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/HomeFragment.java
@@ -1,7 +1,21 @@
 package thelibrarians.sulibraryapp;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by njraf_000 on 9/21/2016.
  */
-public class HomeFragment {
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
 }
--- SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/MainActivity.java
+++ SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/MainActivity.java
@@ -1,13 +1,162 @@
 package thelibrarians.sulibraryapp;
 
+import android.content.res.Configuration;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    DrawerLayout drawer;
+    Toolbar toolbar;
+    FragmentManager fm;
+    FragmentTransaction ft;
+    ActionBarDrawerToggle drawerToggle;
+    FrameLayout frame;
+    ListView navList;
+    String[] listItems;
+
+    //Fragment class instances
+    HomeFragment home = new HomeFragment();
+    AboutFragment about = new AboutFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frame = (FrameLayout) findViewById(R.id.content_container);
+
+        //navigation drawer list view
+        navList = (ListView) findViewById(R.id.drawer_list);
+        listItems = getResources().getStringArray(R.array.nav_items);
+        navList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_view, listItems));
+        navList.setOnItemClickListener(this);
+
+        //navigation drawer setup
+        drawer = (DrawerLayout) findViewById(R.id.drawer);
+        drawerToggle = new ActionBarDrawerToggle(this, drawer, R.string.drawer_open, R.string.drawer_close)
+        {
+
+            public void onDrawerClosed(View view)
+            {
+                supportInvalidateOptionsMenu();
+                //drawerOpened = false;
+            }
+
+            public void onDrawerOpened(View drawerView)
+            {
+                supportInvalidateOptionsMenu();
+                //drawerOpened = true;
+            }
+        };
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawer.addDrawerListener(drawerToggle);  //animate hamburger icon
+        drawerToggle.syncState();
+
+        //set app bar
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        toolbar.setTitle("The title");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        //display home fragment
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        ft.add(R.id.content_container, home).commit();
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.app_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        drawer.openDrawer(GravityCompat.START);
+
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        drawer.closeDrawers();
+
+        return true;
+    }
+
+    // `onPostCreate` called when activity start-up is complete after `onStart()`
+    // NOTE! Make sure to override the method with only a single `Bundle` argument
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //drawer item clicked listener
+
+        ft = fm.beginTransaction();
+
+        switch(position) {
+            case 0:
+                Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                ft.replace(R.id.content_container, home);
+                break;
+            case 1:
+                Toast.makeText(MainActivity.this, "About", Toast.LENGTH_SHORT).show();
+                ft.replace(R.id.content_container, about);
+                break;
+        }
+        //add previous transaction/fragment to stack
+        // so user can go back to it
+        ft.addToBackStack(null).commit();
+
+        drawer.closeDrawers();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        //define function of phone's back button
+        if (this.drawer.isDrawerOpen(GravityCompat.START)) {
+            this.drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
 }
