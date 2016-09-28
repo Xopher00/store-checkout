19e6b088634f2ccf95a2e75f387cef78cb7fa615
AJShoop <ashoop1@gulls.salisbury.edu>
2016-09-28 13:01:27 -0400

    Minor code edits Wednesday meeting

--- SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/MainActivity.java
+++ SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/MainActivity.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
