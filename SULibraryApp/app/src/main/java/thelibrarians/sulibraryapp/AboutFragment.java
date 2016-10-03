6159cfd62f85290ead56e9f9937e7808a483722c
AJShoop <ashoop1@gulls.salisbury.edu>
2016-10-03 12:43:57 -0400

    Got link working, just need to find right link -Andy

--- SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/AboutFragment.java
+++ SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/AboutFragment.java
@@ -2,9 +2,11 @@ package thelibrarians.sulibraryapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class AboutFragment extends Fragment {
 
@@ -12,7 +14,10 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        TextView t= (TextView) view.findViewById(R.id.textView4);
+        t.setMovementMethod(LinkMovementMethod.getInstance());
+        return view;
     }
 
 }
