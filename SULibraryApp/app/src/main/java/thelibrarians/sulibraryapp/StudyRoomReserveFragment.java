3dd851e6c4c8a5ab05e4bf642908ae3e9a2e3fbb
KRIS\Xopher <kprice5@gulls.salisbury.edu>
2016-10-08 15:57:52 -0400

    Created inner class and xml fragment for viewing study rooms

--- SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/StudyRoomReserveFragment.java
+++ SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/StudyRoomReserveFragment.java
@@ -1,5 +1,6 @@
 package thelibrarians.sulibraryapp;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.app.Activity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -22,6 +24,7 @@ import java.util.ArrayList;
 
 public class StudyRoomReserveFragment extends Fragment implements AdapterView.OnItemClickListener {
 
+    static int position;
     ListView listViewsrr; //listView study room reservation
     //array of headers pulled from kris_strings.xml
     String[] sectionHeader;
@@ -124,42 +127,272 @@ public class StudyRoomReserveFragment extends Fragment implements AdapterView.On
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+        this.position = position;
+        Fragment p1; FragmentManager fragmentManager; FragmentTransaction fragmentTransaction;
         //CAUTION: section headers count as positions
         //i.e. position 0 is section header 1
         switch(position) {
             //show respective study room and availability
             case 1://room 139
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             case 2://room 140
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             //case 3 refers to section header 2
             case 4://room 225
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             case 5://room 226
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             case 6://room 227
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             case 7://room 228
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             case 8://room 234
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             case 9://room 235
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             case 10://room 236
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             case 11://room 237
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             case 12://room 238
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             case 13://room 239
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             case 14://room 240
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             case 15://room 241
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             case 16://room 242
+                p1 = new RoomDeetsFragment();
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_container, p1);
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
         }
     }
+
+    public static class RoomDeetsFragment extends Fragment{
+
+        TextView roomName;
+        TextView roomAvail;
+        TextView roomCap;
+        TextView roomLoc;
+        TextView roomTime;
+        TextView roomWall;
+        TextView roomBoard;
+        TextView roomReserve;
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+            View roomView = inflater.inflate(R.layout.fragment_room_deets, container, false);
+
+            roomName = (TextView) roomView.findViewById(R.id.roomName);
+            roomAvail = (TextView) roomView.findViewById(R.id.roomAvail);
+            roomCap = (TextView) roomView.findViewById(R.id.roomCap);
+            roomLoc = (TextView) roomView.findViewById(R.id.roomLoc);
+            roomWall = (TextView) roomView.findViewById(R.id.roomWall);
+            roomTime = (TextView) roomView.findViewById(R.id.roomTime);
+            roomBoard = (TextView) roomView.findViewById(R.id.roomBoard);
+            roomReserve = (TextView) roomView.findViewById(R.id.roomReserve);
+
+            switch(position){
+                    case 1:
+                        roomName.setText("Room 139");
+                        roomAvail.setText("First Come, First Served");
+                        roomReserve.setVisibility(View.GONE);
+                        roomWall.setVisibility(View.GONE);
+                        roomCap.setText("Up to 6 People");
+                        roomLoc.setText("First Floor, near Cafe");
+                        roomBoard.setText("1 Whiteboard");
+                        break;
+                    case 2:
+                        roomName.setText("Room 140");
+                        roomAvail.setText("First Come, First Served");
+                        roomReserve.setVisibility(View.GONE);
+                        roomWall.setVisibility(View.GONE);
+                        roomCap.setText("Up to 6 People");
+                        roomLoc.setText("First Floor, near Cafe");
+                        roomBoard.setText("1 Whiteboard");
+                        break;
+                    case 4:
+                        roomName.setText("Room 225");
+                        roomCap.setText("Up to 4 People");
+                        roomLoc.setText("Second floor, across from ID&D");
+                        roomTime.setVisibility(View.GONE);
+                        roomBoard.setText("1 Whiteboard");
+                        roomWall.setText("Window (facing Henson lawn");
+                        break;
+                    case 5:
+                        roomName.setText("Room 226");
+                        roomCap.setText("Up to 10 People");
+                        roomLoc.setText("Second floor, across from ID&D");
+                        roomTime.setVisibility(View.GONE);
+                        roomBoard.setText("2 Whiteboards");
+                        roomWall.setText("Window (facing Henson lawn");
+                        break;
+                    case 6:
+                        roomName.setText("Room 227");
+                        roomCap.setText("Up to 10 People");
+                        roomLoc.setText("Second floor, across from ID&D");
+                        roomTime.setVisibility(View.GONE);
+                        roomBoard.setText("2 Whiteboards");
+                        roomWall.setVisibility(View.GONE);
+                        break;
+                    case 7:
+                        roomName.setText("Room 228");
+                        roomCap.setText("Up to 4 People");
+                        roomLoc.setText("Second floor, across from ID&D");
+                        roomTime.setVisibility(View.GONE);
+                        roomBoard.setText("2 Whiteboards");
+                        roomWall.setVisibility(View.GONE);
+                        break;
+                    case 8:
+                        roomName.setText("Room 234");
+                        roomCap.setText("Up to 10 People");
+                        roomLoc.setText("Second floor, overlooking atrium");
+                        roomTime.setVisibility(View.GONE);
+                        roomBoard.setText("2 Whiteboards");
+                        roomWall.setText("Glass wall facing atrium");
+                        break;
+                    case 9:
+                        roomName.setText("Room 235");
+                        roomCap.setText("Up to 6 People");
+                        roomLoc.setText("Second floor, overlooking atrium");
+                        roomTime.setVisibility(View.GONE);
+                        roomBoard.setText("2 Whiteboards");
+                        roomWall.setText("Glass wall facing atrium");
+                        break;
+                    case 10:
+                        roomName.setText("Room 236");
+                        roomCap.setText("Up to 10 People");
+                        roomLoc.setText("Second floor, overlooking atrium");
+                        roomTime.setVisibility(View.GONE);
+                        roomBoard.setText("2 Whiteboards");
+                        roomWall.setText("Glass wall facing atrium");
+                        break;
+                    case 11:
+                        roomName.setText("Room 237");
+                        roomCap.setText("Up to 10 People");
+                        roomLoc.setText("Second floor, overlooking atrium");
+                        roomTime.setVisibility(View.GONE);
+                        roomBoard.setText("2 Whiteboards");
+                        roomWall.setText("Glass wall facing atrium");
+                        break;
+                    case 12:
+                        roomName.setText("Room 238");
+                        roomCap.setText("Up to 10 People");
+                        roomLoc.setText("Second floor, overlooking atrium");
+                        roomTime.setVisibility(View.GONE);
+                        roomBoard.setText("2 Whiteboards");
+                        roomWall.setText("Glass wall facing atrium");
+                        break;
+                    case 13:
+                        roomName.setText("Room 239");
+                        roomCap.setText("Up to 8 People");
+                        roomLoc.setText("Second floor, near Cafe");
+                        roomBoard.setText("1 Whiteboard");
+                        roomWall.setText("Glass wall facing atrium");
+                        break;
+                    case 14:
+                        roomName.setText("Room 240");
+                        roomCap.setText("Up to 8 People");
+                        roomLoc.setText("Second floor, near Cafe");
+                        roomBoard.setText("1 Whiteboard");
+                        roomWall.setVisibility(View.GONE);
+                        break;
+                    case 15:
+                        roomName.setText("Room 241");
+                        roomCap.setText("Up to 8 People");
+                        roomLoc.setText("Second floor, near Cafe");
+                        roomBoard.setText("2 Whiteboards");
+                        roomWall.setVisibility(View.GONE);
+                        break;
+                    case 16:
+                        roomName.setText("Room 242");
+                        roomCap.setText("Up to 8 People");
+                        roomLoc.setText("Second floor, near Cafe");
+                        roomBoard.setText("2 Whiteboards");
+                        roomWall.setVisibility(View.GONE);
+                        break;
+                }
+
+            return roomView;
+        }
+    }
 }
+
+
