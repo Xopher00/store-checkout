d91c422723c2291691e4704c93357475c96e0bee
ACER\njraf_000 <njrafala@hotmail.com>
2016-10-31 12:01:44 -0400

    Home page work

--- SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/CalendarFragment.java
+++ SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/CalendarFragment.java
@@ -1,108 +1,134 @@
 package thelibrarians.sulibraryapp;
 
-import android.content.Context;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CalendarFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link CalendarFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * general fragment for HomeFragment's viewpager
  */
+
 public class CalendarFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    int tab;
+    JSONObject jDay;
+    String base_url, full_string;
+    HttpURLConnection conn; // Connection object
+    JSONObject week1;
+    JSONObject week2;
+    ArrayList<JSONObject> myweek;
 
-    private OnFragmentInteractionListener mListener;
-
-    public CalendarFragment() {
-        // Required empty public constructor
+    public CalendarFragment(int position, JSONObject j) {
+        tab = position;
+        jDay = j;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CalendarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CalendarFragment newInstance(String param1, String param2) {
-        CalendarFragment fragment = new CalendarFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public CalendarFragment(int position) {
+        //constructor for testing before adding JSON
+        tab = position;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        base_url = "https://api3.libcal.com/api_hours_grid.php?iid=823&format=json&weeks=2";
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        new JSONRetriever().execute();
+
+        TextView tv = (TextView) v.findViewById(R.id.tv);
+        tv.setText("home page "+tab);
+
+        return v;
+    }
+
+    private void parseJSON() {
+        try {
+            JSONObject j = new JSONObject(full_string);
+            //get info for this week and next week
+            //each week object contains objects for each day of the week
+            week1 = new JSONObject(j.getJSONArray("locations").getString(0)).getJSONArray("weeks").getJSONObject(0);
+            week2 = new JSONObject(j.getJSONArray("locations").getString(0)).getJSONArray("weeks").getJSONObject(1);
+
+            //get today's date
+
+            //day = 6 - (day of week + 6); use Calendar class
+
+            //build array of 7 day period
+            myweek = new ArrayList<JSONObject>();
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    public class JSONRetriever extends AsyncTask<Void, Void, Void> {
+
+        /*
+        * THIS STARTS WHEN JSONRetriever.execute() IS CALLED
+        *
+        * THIS IS STRICTLY FOR GRABBING THE STRING. DO NOT ATTEMPT TO
+        * CALL ANY PARENT CLASS METHODS OR CHANGE ANY UI ELEMENTS IN
+        * THIS METHOD. IT WILL FAIL AND YOU WILL BE SAD. I'M SORRY.
+        * */
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                URL url; // URL object
+                StringBuilder response = new StringBuilder(); // Allows string appending
+                String inputLine; // Buffer for inputStream
+                try {
+                    url = new URL(base_url); // url passed in
+                    try {
+                        conn = (HttpURLConnection)url.openConnection(); // Opens new connection
+                        conn.setConnectTimeout(5000); // Aborts connection if connection takes too long
+                        conn.setRequestMethod("GET"); // Requests to HTTP that we want to get something from it
+                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream())); // BufferedReader object
+                        try {
+                            while ((inputLine = br.readLine()) != null) // While there are more contents to read
+                                response.append(inputLine); // Append the new data to all grabbed data
+                            br.close(); // Close connection
+                        } catch (IOException e) {}
+                    } catch (IOException e) {}
+                } catch (MalformedURLException e) {}
+                full_string = response.toString(); // Sets string in parent class to be the string taken from the URL
+            } catch (Exception e) {}
+            return null;
         }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+        /*
+        * THIS STARTS ONCE doInBackground(...) COMPLETES
+        *
+        * THIS CONTINUES ON THE MAIN THREAD (UI ELEMENTS CAN BE CHANGED)
+        * */
+
+        protected void onPostExecute(Void v){
+            parseJSON();
         }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
--- SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/HomeFragment.java
+++ SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/HomeFragment.java
@@ -1,29 +1,114 @@
 package thelibrarians.sulibraryapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
 
-    View v;
+    View view;
     FragmentManager fm;
+    HomeFragment.SectionsPagerAdapter mSectionsPagerAdapter;
+    ViewPager mViewPager;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        v = inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
         fm =  getActivity().getSupportFragmentManager();
 
         //clear fragment backstack when home page is visited
         if(fm.getBackStackEntryCount() > 0)
             fm.popBackStack(fm.getBackStackEntryAt(0).getId(), FragmentManager.POP_BACK_STACK_INCLUSIVE);
 
-        return v;
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new HomeFragment.SectionsPagerAdapter(getChildFragmentManager());
+        //mSectionsPagerAdapter.getItem()
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) view.findViewById(R.id.frag_pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+
+        return view;
     }
+
+
+
+
+
+    private class SectionsPagerAdapter extends FragmentStatePagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            /*
+            switch(position) {
+                case 0:
+                    return new CalendarFragment(position, myweek.get(position));
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+            }
+            */
+            return new CalendarFragment(position);
+        }
+
+        @Override
+        public int getCount() {
+            return 7;
+        }
+    }
+
+
+
+
 
 }
