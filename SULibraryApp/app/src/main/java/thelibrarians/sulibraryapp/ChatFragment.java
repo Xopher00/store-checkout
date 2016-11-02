5fca8cee4055457b65584d112c86d30583523160
KRIS\Xopher <kprice5@gulls.salisbury.edu>
2016-11-02 19:44:02 -0400

    created chat fragment

--- SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/ChatFragment.java
+++ SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/ChatFragment.java
@@ -1,10 +1,13 @@
 package thelibrarians.sulibraryapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ChatFragment extends Fragment {
 
@@ -12,7 +15,17 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uriUrl = Uri.parse("https://us.libraryh3lp.com/mobile/su-allstaff@chat.libraryh3lp.com?skin=22280&identity=Librarian");//requires login
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+            } ;
+            TextView ct = (TextView) view.findViewById(R.id.chatMeUp);
+            ct.setOnClickListener(listener);
+            return view;
+        }
     }
-
-}
