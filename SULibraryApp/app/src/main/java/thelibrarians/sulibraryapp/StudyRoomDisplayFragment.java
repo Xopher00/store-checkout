bb0e31e79cfb4e78a903dac529a6a75a6b333217
AJShoop <ashoop1@gulls.salisbury.edu>
2016-11-14 15:32:16 -0500

    Comments and cleanups <3

--- SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/StudyRoomDisplayFragment.java
+++ SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/StudyRoomDisplayFragment.java
@@ -92,65 +92,72 @@ public class StudyRoomDisplayFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        roomView = inflater.inflate(R.layout.fragment_study_room_display, container, false);
-        roomAll = (LinearLayout) roomView.findViewById(R.id.study_room_all);
-        roomAll.setVisibility(View.INVISIBLE);
-
+        roomView = inflater.inflate(R.layout.fragment_study_room_display, container, false); // Gets View
+        roomAll = (LinearLayout) roomView.findViewById(R.id.study_room_all); // Gets Layout
+        roomAll.setVisibility(View.INVISIBLE); // Sets View to Invisible until loading is finished
         //create ImageView object
         //assign ImageView id
         roomImage = (ImageView) roomView.findViewById(R.id.roomImage);
         //create TextView Objects
         //assign TextView id's to them
-        roomName = (TextView) roomView.findViewById(R.id.roomName);
-        roomAvail = (TextView) roomView.findViewById(R.id.roomAvail);
-        roomCap = (TextView) roomView.findViewById(R.id.roomCap);
-        roomLoc = (TextView) roomView.findViewById(R.id.roomLoc);
-        roomDescription = (TextView) roomView.findViewById(R.id.roomDescription);
-        roomReserve = (Button) roomView.findViewById(R.id.reserveButton);
+        roomName = (TextView) roomView.findViewById(R.id.roomName); // Assigns TextView from xml
+        roomAvail = (TextView) roomView.findViewById(R.id.roomAvail); // Assigns TextView from xml
+        roomCap = (TextView) roomView.findViewById(R.id.roomCap); // Assigns TextView from xml
+        roomLoc = (TextView) roomView.findViewById(R.id.roomLoc); // Assigns TextView from xml
+        roomDescription = (TextView) roomView.findViewById(R.id.roomDescription); // Assigns TextView from xml
+        roomReserve = (Button) roomView.findViewById(R.id.reserveButton); // Assigns Button from xml
         roomReserve.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String url = new String("http://salisbury.libcal.com/rooms_acc.php?gid=");
-                url = url.concat(groupid);
-                Uri uriUrl = Uri.parse(url);
-                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-                startActivity(launchBrowser);
+            public void onClick(View v) { // OnClick
+                String url = new String("http://salisbury.libcal.com/rooms_acc.php?gid="); // URL
+                url = url.concat(groupid); // Adds groupid to complete link
+                Uri uriUrl = Uri.parse(url); // Parses URL
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl); // Creates intent
+                startActivity(launchBrowser); // Launches intent
             }
         });
-        new JSONRetriever().execute();
+        new JSONRetriever().execute(); // Gets JSON string
         return roomView;
     }
     private void parseJSON(){
         try {
-            JSONObject json_obj = new JSONObject(full_string);
-            JSONObject avail = json_obj.getJSONObject("availability");
-            roomName.setText(avail.getString("name"));
-            roomAvail.setText(calculateAvailability(avail));
-            roomCap.setText(avail.getString("capacity"));
-            roomLoc.setText(avail.getString("directions"));
-            roomDescription.setText(avail.getString("description"));
-            setRoomIcon();
-        }catch(JSONException e){
-            e.printStackTrace();
+            JSONObject json_obj = new JSONObject(full_string); // Initializes JSONObject
+            JSONObject avail = json_obj.getJSONObject("availability"); // Initializes JSONObject for availability
+            roomName.setText(avail.getString("name")); // Gets and sets room name
+            roomAvail.setText(calculateAvailability(avail)); // Gets and sets availability message
+            roomCap.setText(avail.getString("capacity")); // Gets and sets capacity
+            roomLoc.setText(avail.getString("directions")); // Gets and sets directions
+            roomDescription.setText(avail.getString("description")); // Gets and sets room description
+            setRoomIcon(); // Sets icon for each element
+        }catch(JSONException e){ // Displays error message
+            e.printStackTrace(); // ||
         }
-        roomAll.setVisibility(View.VISIBLE);
+        roomAll.setVisibility(View.VISIBLE); // Sets Layout to Visible
     }
+
+    /*
+    * CREATES THE URL FOR RESERVE ROOM SITE
+    * */
     private void createURL(){
-        base_url = "https://api2.libcal.com/1.0/room_availability/?iid=823&room_id=";
-        String[] study_room_ids = getResources().getStringArray(R.array.study_room_ids);
-        String[] group_ids = getResources().getStringArray(R.array.study_room_groups);
-        String id = new String();
-        if(position < 3) {
+        base_url = "https://api2.libcal.com/1.0/room_availability/?iid=823&room_id="; // Beginning of URL
+        String[] study_room_ids = getResources().getStringArray(R.array.study_room_ids); // Gets IDs
+        String[] group_ids = getResources().getStringArray(R.array.study_room_groups); // Gets GroupIDs
+        String id; // Declares string
+        if(position < 3) { // If in one spot prior to position 3 (second header)
             id = study_room_ids[position - 1];
             groupid = group_ids[position - 1];
         }
-        else {
+        else { // If in the other
             id = study_room_ids[position - 2];
             groupid = group_ids[position - 2];
         }
-        base_url = base_url.concat(id);
-        base_url = base_url.concat("&limit=150&extend=1&key=d095e46065538df2f67eb7cf7d483896");
+        base_url = base_url.concat(id); // Adds id to URL
+        base_url = base_url.concat("&limit=150&extend=1&key=d095e46065538df2f67eb7cf7d483896"); // Ends URL
     }
+
+    /*
+    * DETERMINES AVAILABILITY MESSAGE BASED ON JSON REPORT
+    * */
     private String calculateAvailability(JSONObject avail){
         if(position < 3)
             return "First come, first serve";
@@ -167,6 +174,10 @@ public class StudyRoomDisplayFragment extends Fragment{
         }
         return new String();
     }
+
+    /*
+    * SETS IMAGE BASED ON POSITION
+    * */
     private void setRoomIcon(){
         switch(position){
             case 1:
@@ -209,6 +220,8 @@ public class StudyRoomDisplayFragment extends Fragment{
                 roomImage.setImageResource(R.drawable.ac242);
                 break;
         }
+        Log.e("CLOSED","");
     }
+
 }
 
