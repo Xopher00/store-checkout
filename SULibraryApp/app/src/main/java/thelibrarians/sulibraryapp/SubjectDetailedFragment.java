1223ec5d96c6307a53103af60752420b205da82b
Jessica Tang <jessica.tang1421@gmail.com>
2017-02-07 12:53:11 -0500

    research help icon resolutions fixed

--- SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/SubjectDetailedFragment.java
+++ SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/SubjectDetailedFragment.java
@@ -80,7 +80,7 @@ public class SubjectDetailedFragment extends Fragment {
                     titles = getResources().getStringArray(R.array.acct);
                     //imgView.setImageResource(R.drawable.ggrobb);
                     //Referencing the XML that will create the overall header for the page
-                    icon.setImageResource(R.drawable.accounting3x);
+                    icon.setImageResource(R.drawable.accounting);
                     circle.setImageResource(R.drawable.custom_circle_yellow);
                     rectangle.setImageResource(R.drawable.custom_rectangle_yellow);
                     title.setText("Accounting & Legal Studies");
@@ -99,7 +99,7 @@ public class SubjectDetailedFragment extends Fragment {
                 case 1:
                     titles = getResources().getStringArray(R.array.anthro);
                     //img.setImageResource(R.drawable.jlparrigin);
-                    icon.setImageResource(R.drawable.anthropology3x);
+                    icon.setImageResource(R.drawable.anthropology);
                     circle.setImageResource(R.drawable.custom_circle_blue);
                     rectangle.setImageResource(R.drawable.custom_rectangle_blue);
                     title.setText("Anthropology");
@@ -108,7 +108,7 @@ public class SubjectDetailedFragment extends Fragment {
                 case 2:
                     titles = getResources().getStringArray(R.array.ahp);
                     //img.setImageResource(R.drawable.mxchakraborty);
-                    icon.setImageResource(R.drawable.ahp3x);
+                    icon.setImageResource(R.drawable.ahp);
                     circle.setImageResource(R.drawable.custom_circle_green);
                     rectangle.setImageResource(R.drawable.custom_rectangle_green);
                     title.setText("Applied Health & Physiology");
@@ -117,7 +117,7 @@ public class SubjectDetailedFragment extends Fragment {
                 case 3:
                     titles = getResources().getStringArray(R.array.art);
                     //img.setImageResource(R.drawable.cmeckardt);
-                    icon.setImageResource(R.drawable.art3x);
+                    icon.setImageResource(R.drawable.art);
                     circle.setImageResource(R.drawable.custom_circle_blue);
                     rectangle.setImageResource(R.drawable.custom_rectangle_blue);
                     title.setText("Art & Art History");
@@ -126,7 +126,7 @@ public class SubjectDetailedFragment extends Fragment {
                 case 4:
                     titles = getResources().getStringArray(R.array.bio);
                     //img.setImageResource(R.drawable.sebrazer);
-                    icon.setImageResource(R.drawable.biology3x);
+                    icon.setImageResource(R.drawable.biology);
                     circle.setImageResource(R.drawable.custom_circle_green);
                     rectangle.setImageResource(R.drawable.custom_rectangle_green);
                     title.setText("Biology");
@@ -135,7 +135,7 @@ public class SubjectDetailedFragment extends Fragment {
                 case 5:
                     titles = getResources().getStringArray(R.array.bus);
                     //img.setImageResource(R.drawable.ggrobb);
-                    icon.setImageResource(R.drawable.business3x);
+                    icon.setImageResource(R.drawable.business);
                     circle.setImageResource(R.drawable.custom_circle_yellow);
                     rectangle.setImageResource(R.drawable.custom_rectangle_yellow);
                     title.setText("Business");
@@ -143,7 +143,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Chemistry
                 case 6:
                     //img.setImageResource(R.drawable.sebrazer);
-                    icon.setImageResource(R.drawable.chemistry3x);
+                    icon.setImageResource(R.drawable.chemistry);
                     circle.setImageResource(R.drawable.custom_circle_green);
                     rectangle.setImageResource(R.drawable.custom_rectangle_green);
                     title.setText("Chemistry");
@@ -151,7 +151,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Communication Arts
                 case 7:
                     //img.setImageResource(R.drawable.jlparrigin);
-                    icon.setImageResource(R.drawable.comm3x);
+                    icon.setImageResource(R.drawable.comm);
                     circle.setImageResource(R.drawable.custom_circle_blue);
                     rectangle.setImageResource(R.drawable.custom_rectangle_blue);
                     title.setText("Communication Arts");
@@ -159,7 +159,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Computer Science
                 case 8:
                     //img.setImageResource(R.drawable.sebrazer);
-                    icon.setImageResource(R.drawable.compsci3x);
+                    icon.setImageResource(R.drawable.compsci);
                     circle.setImageResource(R.drawable.custom_circle_green);
                     rectangle.setImageResource(R.drawable.custom_rectangle_green);
                     title.setText("Computer Science");
@@ -167,7 +167,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Conflict Analysis & Dispute Resolution
                 case 9:
                     //img.setImageResource(R.drawable.mxchakraborty);
-                    icon.setImageResource(R.drawable.cadr3x);
+                    icon.setImageResource(R.drawable.cadr);
                     circle.setImageResource(R.drawable.custom_circle_blue);
                     rectangle.setImageResource(R.drawable.custom_rectangle_blue);
                     title.setText("Conflict Analysis & Dispute Resolution");
@@ -175,7 +175,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Dance
                 case 10:
                     //img.setImageResource(R.drawable.arprichard);
-                    icon.setImageResource(R.drawable.dance3x);
+                    icon.setImageResource(R.drawable.dance);
                     circle.setImageResource(R.drawable.custom_circle_blue);
                     rectangle.setImageResource(R.drawable.custom_rectangle_blue);
                     title.setText("Dance");
@@ -183,7 +183,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Economics & Finance
                 case 11:
                     //img.setImageResource(R.drawable.ggrobb);
-                    icon.setImageResource(R.drawable.econ3x);
+                    icon.setImageResource(R.drawable.economy);
                     circle.setImageResource(R.drawable.custom_circle_yellow);
                     rectangle.setImageResource(R.drawable.custom_rectangle_yellow);
                     title.setText("Economics & Finance");
@@ -191,7 +191,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Education
                 case 12:
                     //img.setImageResource(R.drawable.saford);
-                    icon.setImageResource(R.drawable.education3x);
+                    icon.setImageResource(R.drawable.education);
                     circle.setImageResource(R.drawable.custom_circle_purple);
                     rectangle.setImageResource(R.drawable.custom_rectangle_purple);
                     title.setText("Education");
@@ -199,7 +199,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Engineering
                 case 13:
                     //img.setImageResource(R.drawable.sebrazer);
-                    icon.setImageResource(R.drawable.engineering3x);
+                    icon.setImageResource(R.drawable.engineering);
                     circle.setImageResource(R.drawable.custom_circle_green);
                     rectangle.setImageResource(R.drawable.custom_rectangle_green);
                     title.setText("Engineering");
@@ -207,7 +207,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //English
                 case 14:
                     //img.setImageResource(R.drawable.jlparrigin);
-                    icon.setImageResource(R.drawable.english3x);
+                    icon.setImageResource(R.drawable.english);
                     circle.setImageResource(R.drawable.custom_circle_blue);
                     rectangle.setImageResource(R.drawable.custom_rectangle_blue);
                     title.setText("English");
@@ -215,7 +215,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //English Language Institute
                 case 15:
                     //img.setImageResource(R.drawable.lhanscom);
-                    icon.setImageResource(R.drawable.eli3x);
+                    icon.setImageResource(R.drawable.eli);
                     circle.setImageResource(R.drawable.custom_circle_red);
                     rectangle.setImageResource(R.drawable.custom_rectangle_red);
                     title.setText("English Language Institute");
@@ -223,7 +223,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Environmental Studies
                 case 16:
                     //img.setImageResource(R.drawable.sebrazer);
-                    icon.setImageResource(R.drawable.environ3x);
+                    icon.setImageResource(R.drawable.environ);
                     circle.setImageResource(R.drawable.custom_circle_green);
                     rectangle.setImageResource(R.drawable.custom_rectangle_green);
                     title.setText("Environmental Studies");
@@ -231,7 +231,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Geography & Geosciences
                 case 17:
                     //img.setImageResource(R.drawable.sebrazer);
-                    icon.setImageResource(R.drawable.geog3x);
+                    icon.setImageResource(R.drawable.geog);
                     circle.setImageResource(R.drawable.custom_circle_green);
                     rectangle.setImageResource(R.drawable.custom_rectangle_green);
                     title.setText("Geography & Geosciences");
@@ -239,7 +239,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Government Information
                 case 18:
                     //img.setImageResource(R.drawable.ggrobb);
-                    icon.setImageResource(R.drawable.govt3x);
+                    icon.setImageResource(R.drawable.govt);
                     circle.setImageResource(R.drawable.custom_circle_red);
                     rectangle.setImageResource(R.drawable.custom_rectangle_red);
                     title.setText("Government Information");
@@ -247,7 +247,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Health & Sport Sciences
                 case 19:
                     //img.setImageResource(R.drawable.cmeckardt);
-                    icon.setImageResource(R.drawable.hss3x);
+                    icon.setImageResource(R.drawable.hss);
                     circle.setImageResource(R.drawable.custom_circle_purple);
                     rectangle.setImageResource(R.drawable.custom_rectangle_purple);
                     title.setText("Health & Sport Sciences");
@@ -255,7 +255,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //History
                 case 20:
                     //img.setImageResource(R.drawable.jlparrigin);
-                    icon.setImageResource(R.drawable.history3x);
+                    icon.setImageResource(R.drawable.history);
                     circle.setImageResource(R.drawable.custom_circle_blue);
                     rectangle.setImageResource(R.drawable.custom_rectangle_blue);
                     title.setText("History");
@@ -263,7 +263,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Information & Decision Sciences
                 case 21:
                     //img.setImageResource(R.drawable.sebrazer);
-                    icon.setImageResource(R.drawable.ids3x);
+                    icon.setImageResource(R.drawable.ids);
                     circle.setImageResource(R.drawable.custom_circle_yellow);
                     rectangle.setImageResource(R.drawable.custom_rectangle_yellow);
                     title.setText("Information & Decision Sciences");
@@ -271,7 +271,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Interdisciplinary Studies
                 case 22:
                     //img.setImageResource(R.drawable.cmeckardt);
-                    icon.setImageResource(R.drawable.inter3x);
+                    icon.setImageResource(R.drawable.inter);
                     circle.setImageResource(R.drawable.custom_circle_red);
                     rectangle.setImageResource(R.drawable.custom_rectangle_red);
                     title.setText("Interdisciplinary Studies");
@@ -279,7 +279,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Management & Marketing
                 case 23:
                     //img.setImageResource(R.drawable.ggrobb);
-                    icon.setImageResource(R.drawable.mgmt3x);
+                    icon.setImageResource(R.drawable.mgmt);
                     circle.setImageResource(R.drawable.custom_circle_yellow);
                     rectangle.setImageResource(R.drawable.custom_rectangle_yellow);
                     title.setText("Management & Marketing");
@@ -287,7 +287,7 @@ public class SubjectDetailedFragment extends Fragment {
                 //Mathematics
                 case 24:
                     //img.setImageResource(R.drawable.sebrazer);
-                    icon.setImageResource(R.drawable.math3x);
+                    icon.setImageResource(R.drawable.math);
                     circle.setImageResource(R.drawable.custom_circle_green);
                     rectangle.setImageResource(R.drawable.custom_rectangle_green);
                     title.setText("Mathematics");
@@ -295,49 +295,49 @@ public class SubjectDetailedFragment extends Fragment {
                 //Medical Laboratory Science
                 case 25:
                     //img.setImageResource(R.drawable.mxchakraborty);
-                    icon.setImageResource(R.drawable.mls3x);
+                    icon.setImageResource(R.drawable.mls);
                     circle.setImageResource(R.drawable.custom_circle_green);
                     rectangle.setImageResource(R.drawable.custom_rectangle_green);
                     title.setText("Medical Laboratory Science");
                     break;
                 //Military Science
                 case 26:
-                    icon.setImageResource(R.drawable.mil3x);
+                    icon.setImageResource(R.drawable.mil);
                     circle.setImageResource(R.drawable.custom_circle_purple);
                     rectangle.setImageResource(R.drawable.custom_rectangle_purple);
                     title.setText("Military Science");
                     break;
                 //Modern Languages
                 case 27:
-                    icon.setImageResource(R.drawable.modlang3x);
+                    icon.setImageResource(R.drawable.modlang);
                     circle.setImageResource(R.drawable.custom_circle_blue);
                     rectangle.setImageResource(R.drawable.custom_rectangle_blue);
                     title.setText("Modern Languages");
                     break;
                 //Music
                 case 28:
-                    icon.setImageResource(R.drawable.music3x);
+                    icon.setImageResource(R.drawable.music);
                     circle.setImageResource(R.drawable.custom_circle_blue);
                     rectangle.setImageResource(R.drawable.custom_rectangle_blue);
                     title.setText("Music");
                     break;
                 //Nursing
                 case 29:
-                    icon.setImageResource(R.drawable.nursing3x);
+                    icon.setImageResource(R.drawable.nursing);
                     circle.setImageResource(R.drawable.custom_circle_green);
                     rectangle.setImageResource(R.drawable.custom_rectangle_green);
                     title.setText("Nursing");
                     break;
                 //Philosophy
                 case 30:
-                    icon.setImageResource(R.drawable.philosophy3x);
+                    icon.setImageResource(R.drawable.philosophy);
                     circle.setImageResource(R.drawable.custom_circle_blue);
                     rectangle.setImageResource(R.drawable.custom_rectangle_blue);
                     title.setText("Philosophy");
                     break;
                 //Physical Education
                 case 31:
-                    icon.setImageResource(R.drawable.physed3x);
+                    icon.setImageResource(R.drawable.physed);
                     circle.setImageResource(R.drawable.custom_circle_purple);
                     rectangle.setImageResource(R.drawable.custom_rectangle_purple);
                     title.setText("Physical Education");
@@ -351,42 +351,42 @@ public class SubjectDetailedFragment extends Fragment {
                     break;
                 //Political Science
                 case 33:
-                    icon.setImageResource(R.drawable.polisci3x);
+                    icon.setImageResource(R.drawable.polisci);
                     circle.setImageResource(R.drawable.custom_circle_blue);
                     rectangle.setImageResource(R.drawable.custom_rectangle_blue);
                     title.setText("Political Science");
                     break;
                 //Psychology
                 case 34:
-                    icon.setImageResource(R.drawable.psychology3x);
+                    icon.setImageResource(R.drawable.psychology);
                     circle.setImageResource(R.drawable.custom_circle_blue);
                     rectangle.setImageResource(R.drawable.custom_rectangle_blue);
                     title.setText("Psychology");
                     break;
                 //Respiratory Therapy
                 case 35:
-                    icon.setImageResource(R.drawable.resp3x);
+                    icon.setImageResource(R.drawable.resp);
                     circle.setImageResource(R.drawable.custom_circle_green);
                     rectangle.setImageResource(R.drawable.custom_rectangle_green);
                     title.setText("Respiratory Therapy");
                     break;
                 //Social Work
                 case 36:
-                    icon.setImageResource(R.drawable.socialwork3x);
+                    icon.setImageResource(R.drawable.socialwork);
                     circle.setImageResource(R.drawable.custom_circle_purple);
                     rectangle.setImageResource(R.drawable.custom_rectangle_purple);
                     title.setText("Social Work");
                     break;
                 //Sociology
                 case 37:
-                    icon.setImageResource(R.drawable.sociology3x);
+                    icon.setImageResource(R.drawable.sociology);
                     circle.setImageResource(R.drawable.custom_circle_blue);
                     rectangle.setImageResource(R.drawable.custom_rectangle_blue);
                     title.setText("Sociology");
                     break;
                 //Theatre
                 case 38:
-                    icon.setImageResource(R.drawable.theatre3x);
+                    icon.setImageResource(R.drawable.theatre);
                     circle.setImageResource(R.drawable.custom_circle_blue);
                     rectangle.setImageResource(R.drawable.custom_rectangle_blue);
                     title.setText("Theatre");
