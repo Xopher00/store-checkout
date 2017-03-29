9ed140f20f10c4e8a82b4ed6619ecd0f1f44c4aa
Andy-PC\Andy <ajs9573@gmail.com>
2017-03-29 15:00:13 -0400

    Adding loading screens and documentation

--- SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/ComputerAvailabilityDisplayFragment.java
+++ SULibraryApp/app/src/main/java/thelibrarians/sulibraryapp/ComputerAvailabilityDisplayFragment.java
@@ -34,48 +34,65 @@ import java.io.*;
 import java.lang.*;
 import java.net.*;
 
+/**
+ * ComputerAvailabilityDisplayFragment:
+ * <br>
+ *  Displays a table which shows the number of computers available and what OS they run
+ *
+ */
 
 public class ComputerAvailabilityDisplayFragment extends Fragment {
 
     int position; // position in array
-    boolean displayed; // If the connection was made and the information is displayed, TRUE, else, FALSE
-    //TableLayout table; //GONNA CHANGE THIS
     ImageView top_img; // The image at the top of the page
     TextView num_computers_available,
             room_description, view_as_map,
-            group_name_text; // TextViews in view
+            group_name_text, loading_msg; // TextViews in view
     SwipeRefreshLayout swipeRefresher; // SwipeRefreshLayout Object
     Integer win_a, win_o, win_u,
         mac_a, mac_o, mac_u,
         lin_a, lin_o, lin_u,
         num_all, num_available; // Integers pulled from the JSON
     String base_url,full_string; // URL and result of the URL
-    HttpURLConnection conn; // Connection object
     DrawerToggleListener toggleListener;
     JSONRetriever jretr;
     View view;
+
+    /**
+     * Default constructor
+     */
 
     public ComputerAvailabilityDisplayFragment() {
         // Required empty public constructor
     }
 
-    /*
-    * Assigns position
-    * */
+    /**
+     * Constructor that assigns position
+     *
+     * @param pos Determines the which part of the previous listview was clicked for data loading
+     */
+
     @SuppressLint("ValidFragment")
     public ComputerAvailabilityDisplayFragment(int pos){
         position = pos;
     }
 
+    /**
+     * Inner class used to start an asynchronous class that reads a JSON stream from a URL and
+     * uses this string in the parent classes methods
+     */
+
     private class JSONRetriever extends AsyncTask<Void, Void, Void>{
 
-        /*
-        * THIS STARTS WHEN JSONRetriever.execute() IS CALLED
-        *
-        * THIS IS STRICTLY FOR GRABBING THE STRING. DO NOT ATTEMPT TO
-        * CALL ANY PARENT CLASS METHODS OR CHANGE ANY UI ELEMENTS IN
-        * THIS METHOD. IT WILL FAIL AND YOU WILL BE SAD. I'M SORRY.
-        * */
+        HttpURLConnection conn; // Connection object
+        /**
+         * THIS STARTS WHEN JSONRetriever.execute() IS CALLED
+         *<br>
+         * THIS IS STRICTLY FOR GRABBING THE STRING. DO NOT ATTEMPT TO
+         * CALL ANY PARENT CLASS METHODS OR CHANGE ANY UI ELEMENTS IN
+         * THIS METHOD. IT WILL FAIL AND YOU WILL BE SAD. I'M SORRY.
+         * @return - null variable of type Void used as an object, not a primitive
+         */
 
         @Override
         protected Void doInBackground(Void... params) {
@@ -102,35 +119,50 @@ public class ComputerAvailabilityDisplayFragment extends Fragment {
             return null;
         }
 
-        /*
-        * THIS STARTS ONCE doInBackground(...) COMPLETES
-        *
-        * THIS CONTINUES ON THE MAIN THREAD (UI ELEMENTS CAN BE CHANGED)
-        * */
+        /**
+         * This starts once doInBackground(...) completes.
+         * <br>
+         * This continues  on the main thread (parent methods that alter the UI can be called at this point).
+         */
 
         protected void onPostExecute(Void v){
             parseJSON();
         }
 
+        /**
+         * This is called once the connection fails.
+         *<br>
+         * This allows nothing to happen.
+         */
+
         protected void onCancelled(){}
     }
+
+    /**
+     * Creates a url with the position and sets displayed to false
+     *
+     * @param savedInstanceState
+     */
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*
-        * FORMATTING THE URL
-        * */
+
         base_url = getActivity().getResources().getString(R.string.json_url); // First part of all URLs
         String[] mapIDs = getResources().getStringArray(R.array.computer_map_ids); // Loads array of possible room IDs
         base_url = base_url.concat(mapIDs[position]); // Adds room IDs to
-        displayed = false; // visuals are not displayed at this point
-        /*
 
-        * CONNECT TO URL
-         *  */
         new JSONRetriever().execute(); // Starts ASync Task
     }
+
+    /**
+     * Initially creates the view
+     *
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return the view for the fragment
+     */
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -142,6 +174,7 @@ public class ComputerAvailabilityDisplayFragment extends Fragment {
         room_description = (TextView) view.findViewById(R.id.computer_room_description); // Assigns Text object
         group_name_text = (TextView) view.findViewById(R.id.group_name_detail); // Assigns Text object
         view_as_map = (TextView) view.findViewById(R.id.view_as_map_computer); // Assigns Text object
+        loading_msg = (TextView) view.findViewById(R.id.comp_display_loading);
         LinearLayout computer_table = (LinearLayout) view.findViewById(R.id.computer_table);
         computer_table.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -164,6 +197,8 @@ public class ComputerAvailabilityDisplayFragment extends Fragment {
             }
         });
 
+        swipeRefresher.setVisibility(View.INVISIBLE);
+
         String map_url = new String("<a href=\""); // Beginning of hyperlink
         map_url = map_url.concat(getResources().getString(R.string.view_as_map_computer_url)); // middle of hyperlink
         String[] map_ids = getResources().getStringArray(R.array.computer_map_ids); // map id of room
@@ -172,13 +207,15 @@ public class ComputerAvailabilityDisplayFragment extends Fragment {
         view_as_map.setText(Html.fromHtml(map_url)); // Sets text to hyperlink
         view_as_map.setMovementMethod(LinkMovementMethod.getInstance()); // Sets hyperlink to lead to a link
 
-        //table.setVisibility(View.INVISIBLE); // Sets image table as Invisible
-        view_as_map.setVisibility(View.INVISIBLE); // Sets View As Map as Invisible
         toggleListener = (DrawerToggleListener) getActivity();
         toggleListener.toggleDrawer(false);
-        view.findViewById(R.id.computer_table).setVisibility(View.INVISIBLE);
         return view;
     }
+
+    /**
+     * Standard onDetach, cancels the connection, allows the drawer to open
+     */
+
     @Override
     public void onDetach() {
         super.onDetach();
@@ -186,10 +223,13 @@ public class ComputerAvailabilityDisplayFragment extends Fragment {
         toggleListener.toggleDrawer(true);
     }
 
+    /**
+     * Standard onStart, sets displayed to false, and starts the connection
+     */
+
     @Override
     public void onStart(){
         super.onStart();
-        displayed = false; // visuals are not displayed at this point
         /*
 
         * CONNECT TO URL
@@ -197,6 +237,10 @@ public class ComputerAvailabilityDisplayFragment extends Fragment {
         jretr = new JSONRetriever();
         jretr.execute(); // Starts ASync Task
     }
+
+    /**
+     * Grabs information from the json pull
+     */
 
     private void parseJSON(){
         JSONObject j; // Declares JSONObject
@@ -226,6 +270,10 @@ public class ComputerAvailabilityDisplayFragment extends Fragment {
 
         fillGrid();
     }
+
+    /**
+     * Fills the information in the grid
+     */
 
     private void fillGrid() {
         room_description.setText(getResources().getStringArray(R.array.computer_room_descriptions)[position]); // Sets description
@@ -290,9 +338,15 @@ public class ComputerAvailabilityDisplayFragment extends Fragment {
             fragmentTransaction.replace(R.id.content_container, fragment);
             fragmentTransaction.addToBackStack(null).commit();
         }
-        view.findViewById(R.id.computer_table).setVisibility(View.VISIBLE);
-        view_as_map.setVisibility(View.VISIBLE); //View As Map = VISIBLE
+        loading_msg.setVisibility(View.INVISIBLE);
+        swipeRefresher.setVisibility(View.VISIBLE);
     }
+
+    /**
+     * Determines if the network connection is available
+     *
+     * @return if a network connection is available
+     */
 
     private boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager
